f59d5dcb9a6da0e0a52ab2566c960b77544dc144
Mart-Dan-Rossi <martinrossi1993@gmail.com>
2024-12-18 17:41:09 -0300

    Adaptando a Mateo Shoes día 1

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
 	images: {
 		domains: ["res.cloudinary.com"],
 	},
+	eslint: {
+		dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+	},
 };
