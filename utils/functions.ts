8aa9d4701c0a3d4a173f8219fdef75925cc22edc
Mart-Dan-Rossi <martinrossi1993@gmail.com>
2024-12-22 22:26:17 -0300

    Day 2

--- utils/functions.ts
+++ utils/functions.ts
@@ -12,10 +12,8 @@ export function getSizeName(
 		Object.keys(product.sizeOptions).forEach((sizeOption) => {
 			const option = sizeOption as keyof ValidSizeOptions;
 			if (
-				product &&
-				product.sizeOptions &&
 				product.sizeOptions[option] &&
-				(product.sizeOptions[option].length ?? 0) > 0
+				(product?.sizeOptions[option]?.length ?? 0) > 0
 			) {
 				displayableSizeName = option;
 			}
