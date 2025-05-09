5c25e08cb6957a215b2b619c77a829758ebcfd32
Mart-Dan-Rossi <martinrossi1993@gmail.com>
2025-05-09 20:44:25 -0300

    9/5 update

--- utils/functions.ts
+++ utils/functions.ts
@@ -145,7 +145,7 @@ export function getAvailableQuantitiesBySizeAndColor(
 		?.filter((reserved) => reserved.color.toLowerCase() === color.toLowerCase())
 		.forEach((reserved) => {
 			const key = reserved.usSize as SizeKey;
-			if (sizeMap[key] && sizeMap[key] !== undefined && reserved.quantity) {
+			if (sizeMap[key] !== undefined && sizeMap[key] && reserved.quantity) {
 				sizeMap[key] -= reserved.quantity;
 			}
 		});
